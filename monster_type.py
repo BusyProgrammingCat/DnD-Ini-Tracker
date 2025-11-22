ff92a21492e6adfe3628fbc0fa9900fe6965050a
BusyCat <gameworldofartur@gmail.com>
2025-11-22 16:12:04 +0100

    III. Premade to Monster Type
    > The Premade Monster idea is renamed into Monster Type and turned into a dedicated class
    > CSV is being used to save data

---
+++ monster_type.py
@@ -0,0 +1,23 @@
+import csv
+import dice
+
+monster_type_list = []
+
+class monster_type:
+    def __init__(self, monster_name, const_hp, hp_dice, death_abi):
+        self.MONSTER_NAME = monster_name
+        self.CONST_HP = const_hp
+        self.HP_DICE = dice.set_dices(hp_dice)
+        self.DEATH_ABI = death_abi
+
+with open("monster_type_set.csv", "r", newline="") as save_file:
+    saved_data = csv.reader(save_file, delimiter=";")
+    for monster in saved_data:
+        
+        monster_type_list.append(monster_type(monster[0], monster[1], monster[2], monster[3]))
+
+def save_monster_types():
+    pass
+
+def add_monster_type():
+    pass
