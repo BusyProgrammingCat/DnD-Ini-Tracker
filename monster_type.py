fced1881ca9e0963a2e3e7aaecdfe2cb1e731319
BusyCat <gameworldofartur@gmail.com>
2025-11-23 01:07:48 +0100

    IIIV. Save your monster typr
    > Adds funtion to save all active monster types

--- monster_type.py
+++ monster_type.py
@@ -7,17 +7,19 @@ class monster_type:
     def __init__(self, monster_name, const_hp, hp_dice, death_abi):
         self.MONSTER_NAME = monster_name
         self.CONST_HP = const_hp
-        self.HP_DICE = dice.set_dices(hp_dice)
+        self.HP_DICE = hp_dice
         self.DEATH_ABI = death_abi
 
 with open("monster_type_set.csv", "r", newline="") as save_file:
     saved_data = csv.reader(save_file, delimiter=";")
     for monster in saved_data:
-        
         monster_type_list.append(monster_type(monster[0], monster[1], monster[2], monster[3]))
 
 def save_monster_types():
-    pass
+    with open("test_monster_type_set.csv", "w", newline="") as save_file:
+        written_data = csv.writer(save_file, delimiter=";")
+        for monster in monster_type_list:
+            written_data.writerow([monster.MONSTER_NAME, monster.CONST_HP, monster.HP_DICE, monster.DEATH_ABI])
 
 def add_monster_type():
     pass
